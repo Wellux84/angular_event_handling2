961c087b4767ec48bbec5758d8024b7dc69844e9
Veli-Pekka Luoto <jullesammakka@hotmail.fi>
2026-05-31 00:04:49 +0300

    assign 4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CurrencyPipe, DatePipe, PercentPipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CurrencyPipe, DatePipe, PercentPipe, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular_event_handling2');
+  title: string = "Putkia"
+  price: number = 1234.5;
+  today: Date = new Date();
+  percent: number = 0.3495;
 }
