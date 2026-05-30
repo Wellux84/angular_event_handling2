397acb5cf4024b0ea355d5184c03b0c82bc2cbf6
Veli-Pekka Luoto <jullesammakka@hotmail.fi>
2026-05-31 00:52:33 +0300

    assign 7

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CurrencyPipe, DatePipe, PercentPipe, UpperCasePipe } from '@angular/common';
 import { Jsoncomp } from './jsoncomp/jsoncomp';
+import { MypipePipe } from './mypipe-pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CurrencyPipe, DatePipe, PercentPipe, UpperCasePipe, Jsoncomp],
+  imports: [RouterOutlet, CurrencyPipe, DatePipe, PercentPipe, UpperCasePipe, Jsoncomp, MypipePipe, CommonModule  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,4 +16,15 @@ export class App {
   price: number = 1234.5;
   today: Date = new Date();
   percent: number = 0.3495;
+  pipetext: string = "";
+  pipedtext: string = "";
+
+  pipeTextInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.pipetext = inputElement.value;
+  }
+
+  showPipeText() {
+    this.pipedtext = this.pipetext;
+  }
 }
